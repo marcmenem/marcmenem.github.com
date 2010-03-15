c3913029ff38e9372b5ad2fe2cfbc05b208886bb
Marc Menem <marc.menem@m4x.org>
2010-03-15 02:43:31 +0100

    Clean up

--- python/vgg_ui/Contents.m
+++
@@ -1,5 +0,0 @@
-% VGG User Interface Library
-%
-% GUI's
-%   vgg_gui_F            - Visualizes epipolar geometry between two views
-%   vgg_gui_H            - Visualizes a homography between two views
